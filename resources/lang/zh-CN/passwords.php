0d943f03a8db33001e5ec8b03c99b902eb7d6c4f
张皓 <zh11@meitu.com>
2018-09-26 15:58:24 +0800

    增加中文语言包

---
+++ resources/lang/zh-CN/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码至少6个字符，并且必须和确认密码一致。',
+    'reset' => '您的密码已经重置!',
+    'sent' => '密码重置链接已发送至您的邮箱!',
+    'token' => '密码重置口令有误。',
+    'user' => "该邮箱地址还未注册。",
+
+];
