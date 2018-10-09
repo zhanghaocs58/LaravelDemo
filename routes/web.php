a357a9147aad75e640663485bca45404a7d58cfa
张皓 <zh11@meitu.com>
2018-10-09 19:49:48 +0800

    debug

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ Route::redirect('/','register/hello');
 //Route::get('register/hello', 'Auth\RegisterController@hello');
 
 Route::group(['namespace' => 'Auth','prefix' => 'auth'],function (){
-    Route::post('/register/hello', 'RegisterController@hello');
-    Route::post('/register/test', 'RegisterController@test');
+    Route::post('register/hello', 'RegisterController@hello');
+    Route::post('register/test', 'RegisterController@test');
 });
